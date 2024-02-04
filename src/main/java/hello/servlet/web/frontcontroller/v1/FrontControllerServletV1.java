d91186d80f9fb8948cf51ce64784825d36add7a4
thstkddnr20 <thstkddnr20@naver.com>
2024-02-04 12:51:02 +0900

    mvc 프레임워크 구현부터 시작(frontcontroller 폴더)
    V1
    기존 구조를 그대로 유지하되, 모든 요청을 한 FrontControllerServletV1에서 받아 처리
    인터페이스로 구현하여 다형성을 이용

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,39 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*") // v1아래의 하위 요청은 다 이 서블릿에서 받아들인다.
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() { // 기본생성자로 매핑정보들 넣어놓음
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV1 controller = controllerMap.get(requestURI); // 인터페이스를 이용하여 ControllerV1으로 가져옴 (다형성)
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(request,response); //가져온 컨트롤러의 process 실행
+    }
+}
